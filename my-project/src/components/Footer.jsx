1c928245b0a389cf681b3011680ddb7b3ff50c67
aryavish54 <aryanvishwakarma828@gmail.com>
2024-12-14 17:03:13 +0530

    Update Footer.jsx

--- my-project/src/components/Footer.jsx
+++ my-project/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-8 text-center">
-      <p>&copy; 2024 Fitness Tracker. All Rights Reserved.</p>
+      <p>&copy; 2024 AR Fitness Tracker. All Rights Reserved.</p>
     </footer>
   );
 }

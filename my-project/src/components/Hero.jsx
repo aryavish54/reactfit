72e85d24b80116e1905312c4898be89b5949d521
aryavish54 <aryanvishwakarma828@gmail.com>
2024-12-15 12:22:30 +0530

    Update Hero.jsx

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Hero() {
   return (
-    <section className="bg-green-600 text-white text-center py-32">
+    <section className="bg-green-500 text-white text-center py-60">
       <div className="container mx-auto">
-        <h2 className="text-4xl font-semibold">Welcome to Fitness Tracker</h2>
-        <p className="mt-12 text-2xl">Track your workouts and stay motivated every day!!</p>
+        <h2 className="text-6xl font-semibold">Welcome to Fitness Tracker</h2>
+        <p className="mt-12 text-4xl">Track your workouts and stay motivated every day!!</p>
       </div>
     </section>
   );
